e55d3bdcafc9c27e0577dee87d0ddcbd24e3879f
Sameer Saleem <sameersaleemq75@gmail.com>
2023-12-20 15:15:50 +0500

    section-2 done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import HeroSection from './components/HeroSection';
+// import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import Section2 from "./components/Section2";
+import Future from "./components/Future";
 
 function App() {
   return (
-    
     <div>
-      <Header />
-      <Routes>
-        <Route path='/HeroSection' element= {<HeroSection/>} />
-      </Routes>
-      <Footer/>
+      <Navbar />
+
+      <HeroSection />
+      <Section2 />
+      <Future />
+      <Footer />
     </div>
-  
   );
 }
 
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import mockup from "../assets/img/Mockup.png";
+import { Image } from "react-bootstrap";
 
 export default function HeroSection() {
   return (
-    <div id='HeroSection'>
-      <h1>This is the HeroSection</h1>
+    <div div style={{ backgroundColor: "#5a3f75" }}>
+      <div className="herosection">
+        <div>
+          <h1>
+            Web3Makr Redefining <br />
+            How You Create
+          </h1>
+          <p>
+            Unlock the blockchain potential to create <br /> Web3 Applications
+          </p>
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Image src={mockup} width={"900px"} />
+      </div>
     </div>
-  )
+  );
 }
